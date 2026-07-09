d7d92390aa32d2b43080cbd9864c06533d512e61
ramonreinabosonit <ramon.reina@bosonit.com>
2026-07-09 13:58:51 +0200

    Ejercicio spark terminado

--- dags/airflow6.py
+++ dags/airflow6.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.bash import BashOperator
+from airflow.decorators import task
 
 with DAG(
     dag_id="airflow6-spark",
@@ -11,11 +12,22 @@ with DAG(
     
 ) as dag:
     
-    t1 = BashOperator(
-        task_id="tarea1",
+    @task()
+    def tarea1():
+        print("Ejecutando tarea 1")
+        
+    @task()
+    def tarea2():
+        print("Ejecutando tarea 2")
+    
+    t1 = tarea1()
+    t2 = tarea2()
+    
+    t3 = BashOperator(
+        task_id="tarea3",
         # bash_command="java -jar /opt/airflow/dags/scripts/Pruebaa.jar"
         # bash_command="spark-submit /opt/airflow/dags/spark/spark.py"
         bash_command="spark-submit --master local[*] /opt/airflow/dags/spark/spark.py"
     )
     
-    t1
+    [t1, t2] >> t3
